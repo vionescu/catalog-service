23eb2e05f1ffcd271a0d4b616e16e793476aeb07
victorionescu <vionescu@gmail.com>
2026-09-10 17:00:53 +0300

    initial project

---
+++ src/main/java/com/victor/catalogservice/CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.victor.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+     static void main(String[] args) {
+        SpringApplication.run(CatalogServiceApplication.class, args);
+    }
+
+}
