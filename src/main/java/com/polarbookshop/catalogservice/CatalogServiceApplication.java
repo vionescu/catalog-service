05c578b47ad58dafff7a378e91d96820db3153b4
victorionescu <vionescu@gmail.com>
2026-09-15 14:54:49 +0300

    chapter 4

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -2,11 +2,13 @@ package com.polarbookshop.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
-     static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(CatalogServiceApplication.class, args);
     }
 
